74bc8e1116a15cb53fdbae3d82db558c8ee44190
Arktos <kkruschi@t-online.de>
2026-03-19 08:33:47 +0100

    replaced move with moveq command

--- Strings/strlen/strlen.s
+++ Strings/strlen/strlen.s
@@ -3,7 +3,7 @@
 ** <- d0 = string length
 
 strlen:                 movem.L a0,-(sp)        ; save registers
-                        move.L  #0,d0           ; string length counter
+                        moveq.L #0,d0           ; string length counter
 loop:                   tst.b   (a0)+           ; test if character = 0
                         beq.s   done            ; yes -> string end
                         addq.l  #1,d0           ; add 1 to string lenhth counter
